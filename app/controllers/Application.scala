00734c1abddff2e3f10cee0ce519d99f8b391af1
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-01-05 14:12:21 +0900

    support add user view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,24 @@
 package controllers
 
+import javax.inject.Inject
+
 import infrastructure.UserDao
 import model.User
 import play.api._
 import play.api.data.Forms._
 import play.api.data._
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfig}
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 import slick.driver.JdbcProfile
 
-class Application extends Controller with HasDatabaseConfig[JdbcProfile] {
+import scala.concurrent.Future
+
+class Application @Inject()(val messagesApi: MessagesApi)
+  extends Controller
+  with HasDatabaseConfig[JdbcProfile]
+  with I18nSupport {
 
   val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
 
@@ -19,26 +27,26 @@ class Application extends Controller with HasDatabaseConfig[JdbcProfile] {
   val form = Form(
     tuple(
       "name" -> nonEmptyText,
-      "mail" -> nonEmptyText
+      "mail" -> email
     )
   )
 
   def index = Action.async { implicit request =>
     userDao.findAll().map { users =>
-      Ok(views.html.index(form, users))
+      Ok(views.html.index(users))
     }
+  }
+
+  def add = Action { implicit request =>
+    Ok(views.html.add(form))
   }
 
   def create = Action.async { implicit request =>
     form.bindFromRequest.fold(
-      errors => {
-        println(errors)
-        userDao.findAll().map { users =>
-          BadRequest(views.html.index(form, users))
-        }
-      },
-      t => {
-        val user = User(id = None, name = t._1, mail = t._2)
+      errors =>
+        Future.successful(BadRequest(views.html.add(errors))),
+      input => {
+        val user = User(id = None, name = input._1, mail = input._2)
         userDao.insert(user).map(_ => Redirect(routes.Application.index))
       }
     )
