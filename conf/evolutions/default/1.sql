9d214e43fa08548d88f139d8673b0e0399aa8d8e
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-01-20 12:56:47 +0900

    fix evolution file

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,8 +4,6 @@ CREATE TABLE "user" (
     "id" bigint(20) NOT NULL AUTO_INCREMENT,
     "name" varchar(255),
     "mail" varchar(255),
---     "created_date" timestamp,
---     "updated_date" timestamp,
     PRIMARY KEY ("id")
 );
 
